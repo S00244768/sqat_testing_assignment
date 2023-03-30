8c0b7979e45fce56083834fa54a284a82a06e09c
S00244755 <123370949+S00244755@users.noreply.github.com>
2023-03-30 13:31:29 +0100

    creation code de base (non corrige) et interface

---
+++ Software Quality and Testing Assignment/DiscountService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_Quality_and_Testing_Assignment
+{
+    public interface DiscountService
+    {
+        double GetDiscount();
+    }
+    public class ImplementationClass : DiscountService
+    {
+        double DiscountService.GetDiscount()
+        {
+            throw new NotImplementedException();
+        }
+
+        static void Main()
+        {
+            DiscountService obj = new ImplementationClass();
+
+            obj.GetDiscount();
+        }
+    }
+}
