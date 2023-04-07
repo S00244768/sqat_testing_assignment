44e4d6c3bfca2abdc4c1ffbd67cc4d5517ac3b97
Sofiane <sofiane.meftah@epita.fr>
2023-04-07 18:07:15 +0200

    Add OnPost form handler and Result display

--- InsuranceService.Web/Pages/CalcPremium.cshtml.cs
+++ InsuranceService.Web/Pages/CalcPremium.cshtml.cs
@@ -1,19 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace InsuranceService.Web.Pages
-{
+{ 
+    class NoDiscountService : DiscountService
+    {
+        public double GetDiscount()
+        {
+            return 1;
+        }
+    }
+    
     public class PrivacyModel : PageModel
     {
+        private InsuranceService ins;
         private readonly ILogger<PrivacyModel> _logger;
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
+            ins = new InsuranceService(new NoDiscountService());
         }
 
         public void OnGet()
         {
+        }
+        
+        [BindProperty]
+        public int Age { get; set; }
+        [BindProperty]
+        public string Location { get; set; }
+        public void OnPost()
+        {
+            var premium = ins.CalcPremium(Age, Location);
+             ViewData["premium"] = premium;
+            _logger.LogInformation($"=> CalcPremium({Age}, {Location}) = {premium}");
         }
     }
 }
