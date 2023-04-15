d939f4ae0669feb7df3d8fd0e667f665b61e8517
Sofiane Meftah <S00244768@atu.ie>
2023-04-15 17:33:27 +0100

    Add selenium test fixture

---
+++ InsuranceService.Web/Runner.cs
@@ -0,0 +1,35 @@
+﻿namespace InsuranceService.Web
+{
+    public class Runner
+    {
+        public static void Run(string url, string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
+            {
+                WebRootPath = "../../../../InsuranceService.Web/wwwroot",
+                ContentRootPath = "../../../../InsuranceService.Web/Pages",
+                Args = args
+            });
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.RunAsync(url);
+        }
+    }
+}
